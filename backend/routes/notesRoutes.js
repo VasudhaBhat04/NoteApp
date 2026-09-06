af49203f703bf1bf0699a14d41d9c0a611899621
vasudha <vasudhabhatkp@gmail.com>
2026-09-06 16:37:46 +0530

    Controllers

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,3 +1,11 @@
-import express from "express"
+import express, { Router } from "express"
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesControllers";
 
 const router = express.Router();
+
+router.get("/",getAllNotes);
+router.post("/",createNote);
+router.put("/:id",updateNote);
+router.delete("/:id",deleteNote);
+
+export default Router;
